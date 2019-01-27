359be3e5f43b17afc2bd04180dd4d7b248a003a9
Morteza Khaki <mortezakhaki@Mortezas-MacBook-Air.local>
2019-01-27 01:13:07 -0500

    react_objects_assignment

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,20 +1,63 @@
 import React, { Component } from "react";
 import "./App.css";
 import Feed from "./Feed.js";
-import allposts from "./blog_posts.json";
+import allPosts from "./blog_posts.json";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      allposts: allposts
+      allPosts: allPosts,
+      newPost: {
+        title: "",
+        body: ""
+      }
     };
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleTextChange = event => {
+    let newPost = { ...this.state.newPost };
+    newPost[event.target.name] = event.target.value;
+    this.setState({ newPost });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const { allPosts, newPost } = this.state;
+
+    if (newPost.title && newPost.body) {
+      const oldPosts = [...this.state.allPosts];
+      oldPosts.push({ id: allPosts.length + 1, ...newPost });
+      // console.log("this is old", oldPosts);
+      this.setState({
+        allPosts: oldPosts,
+        newPost: {
+          title: "",
+          body: ""
+        }
+      });
+    } else if (!newPost.title || !newPost.body) {
+      return <p>please enter valid input</p>;
+    }
+  };
+
   render() {
+    console.log(this.state);
+    const { allPosts } = this.state;
+    const { title, body } = this.state.newPost;
     return (
       <>
         <h1>My Blog</h1>
-        <Feed allposts={this.state.allposts} />
+        <Feed
+          allPosts={allPosts}
+          handleTextChange={this.handleTextChange}
+          handleSubmit={this.handleSubmit}
+          postTitle={title}
+          postBody={body}
+        />
       </>
     );
   }
--- blog/src/Feed.js
+++ blog/src/Feed.js
@@ -1,13 +1,33 @@
 import React from "react";
 import Post from "./Post";
+import PostForm from "./PostForm";
 
-const Feed = props => {
-  console.log(props.allposts);
-  const { allposts } = props;
-  const posts = allposts.map(post => {
-    return <Post id={post.id} title={post.title} body={post.body} />;
+const Feed = ({
+  allPosts,
+  handleTextChange,
+  handleSubmit,
+  postTitle,
+  postBody
+}) => {
+  // console.log(props.allposts);
+
+  const posts = allPosts.map(post => {
+    return (
+      <Post key={post.id} id={post.id} title={post.title} body={post.body} />
+    );
   });
-  return <>{posts}</>;
+  return (
+    <>
+      {posts}
+      <br />
+      <PostForm
+        postTitle={postTitle}
+        postBody={postBody}
+        handleTextChange={handleTextChange}
+        handleSubmit={handleSubmit}
+      />
+    </>
+  );
 };
 
 export default Feed;
